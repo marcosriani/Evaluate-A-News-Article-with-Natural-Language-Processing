d3e9885e1e5ea736c16a762f6b76b6ee7c1de1f1
Marcos Riani <marcosriani@hotmail.com>
2020-06-06 20:44:24 +0100

    Service work working on Prod mode but showing up error in Dev mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -76,6 +76,30 @@ module.exports = {
       filename: './index.html',
     }),
     new MiniCssExtractPlugin({ filename: '[name].css' }),
-    new WorkboxPlugin.GenerateSW(),
+    new WorkboxPlugin.GenerateSW({
+      // Do not precache images
+      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+      // Define runtime caching rules.
+      runtimeCaching: [
+        {
+          // Match any request that ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+          // Apply a cache-first strategy.
+          handler: 'CacheFirst',
+
+          options: {
+            // Use a custom cache name.
+            cacheName: 'images',
+
+            // Only cache 10 images.
+            expiration: {
+              maxEntries: 10,
+            },
+          },
+        },
+      ],
+    }),
   ],
 };
